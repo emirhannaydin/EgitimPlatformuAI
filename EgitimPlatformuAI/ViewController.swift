1fa24ceb5365aa37b641e60d341091b03c6d643e
emirhannaydin <emirhanaydin_1600@hotmail.com>
2025-01-16 14:20:08 +0300

    Initial Commit

---
+++ EgitimPlatformuAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EgitimPlatformuAI
+//
+//  Created by Emirhan Aydın on 16.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
