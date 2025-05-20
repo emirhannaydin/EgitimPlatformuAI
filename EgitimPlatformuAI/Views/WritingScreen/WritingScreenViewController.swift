3ddd8d3ef6c4d7879757b6256f4d5a95acbf8aad
basarnoyan <noyan533@gmail.com>
2025-05-20 18:57:49 +0300

    scollview added

--- EgitimPlatformuAI/Views/WritingScreen/WritingScreenViewController.swift
+++ EgitimPlatformuAI/Views/WritingScreen/WritingScreenViewController.swift
@@ -10,20 +10,51 @@ import UIKit
 
 final class WritingScreenViewController: UIViewController {
     var viewModel: WritingScreenViewModel?
-    
+    @IBOutlet var scrollView: UIScrollView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.isNavigationBarHidden = false
-    }
-    override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            
-        self.navigationController?.isNavigationBarHidden = false
-        }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
         
-        self.navigationController?.isNavigationBarHidden = false
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+            tapGesture.cancelsTouchesInView = false // Eğer butonlar veya başka kontroller varsa dokunmayı engellemesin diye
+            view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        
+        let keyboardHeight = keyboardFrame.height
+        scrollView.contentInset.bottom = keyboardHeight
+        scrollView.verticalScrollIndicatorInsets.bottom = keyboardHeight
+        
+        if let activeField = findFirstResponder(in: self.view) {
+            let activeFieldFrame = activeField.convert(activeField.bounds, to: scrollView)
+            scrollView.scrollRectToVisible(activeFieldFrame, animated: true)
+        }
+    }
+
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        scrollView.contentInset.bottom = 0
+        scrollView.verticalScrollIndicatorInsets.bottom = 0
+    }
+    
+    private func findFirstResponder(in view: UIView) -> UIView? {
+        if view.isFirstResponder {
+            return view
+        }
+        for subview in view.subviews {
+            if let responder = findFirstResponder(in: subview) {
+                return responder
+            }
+        }
+        return nil
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
 }
